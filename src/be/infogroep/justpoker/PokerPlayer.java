29f7f44dec39bae46aa3010cdc58639a6970e0a0
Rik Vanmechelen <rik.vanmechelen@gmail.com>
2012-12-01 23:44:59 +0100

    added states

--- src/be/infogroep/justpoker/PokerPlayer.java
+++ src/be/infogroep/justpoker/PokerPlayer.java
@@ -2,14 +2,26 @@ package be.infogroep.justpoker;
 
 import com.esotericsoftware.kryonet.Connection;
 
+import edu.vub.at.commlib.PlayerState;
+
 public class PokerPlayer {
 	private String name;
 	private Connection connection;
 	private int id;
+	private volatile PlayerState state;
 	
+	public PlayerState getState() {
+		return state;
+	}
+
+	public void setState(PlayerState state) {
+		this.state = state;
+	}
+
 	public PokerPlayer(int i, Connection c){
 		this.id = i;
 		this.connection = c;
+		this.state = PlayerState.Unknown;
 	}
 
 	public String getName() {
---
+++ src/edu/vub/at/commlib/PlayerState.java
@@ -0,0 +1,6 @@
+package edu.vub.at.commlib;
+
+public enum PlayerState {
+	Bet, Fold, Check, AllIn, // From Client
+	Unknown              // For Server: 'undecided'
+}
