c44cccbdede0fe2716a2f405f837d230808d82ad
Roeland Matthijssens <fulgens.ailurus@gmail.com>
2012-12-04 00:49:26 +0100

    images move over border of layout when animated

--- src/be/infogroep/justpoker/ServerTableActivity.java
+++ src/be/infogroep/justpoker/ServerTableActivity.java
@@ -52,8 +52,8 @@ public class ServerTableActivity extends Activity {
 			public void onLeftToRight() {
 				runOnNotUiThread(new Runnable() {
 					public void run() {
-						//clearTable();
-						cps.startNewGame();
+						clearTable();
+						//cps.startNewGame();
 					}
 				});
 
@@ -63,8 +63,8 @@ public class ServerTableActivity extends Activity {
 			public void onRightToLeft() {
 				runOnNotUiThread(new Runnable() {
 					public void run() {
-						//clearTable();
-						cps.startNewGame();
+						clearTable();
+						//cps.startNewGame();
 					}
 				});
 			}
@@ -334,7 +334,7 @@ public class ServerTableActivity extends Activity {
 			@TargetApi(16)
 			public void onAnimationEnd(Animation animation) {
 				card.setImageDrawable(null);
-	            Log.d("justPoker - Client", "---- animation end listener called"  );
+	            Log.d("justPoker - Client", "---- animation end listener called" );
 			}
 
 			@Override
